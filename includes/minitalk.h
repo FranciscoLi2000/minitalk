edf09cb301b64271b2ebaf89b93612fd2a223f96
FranciscoLi2000 <francisco2000li@icloud.com>
2025-03-12 19:26:02 +0000

    Parte Obligatoria

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -1,27 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yufli <yufli@student.42barcelona.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/10 10:31:29 by yufli             #+#    #+#             */
+/*   Updated: 2025/03/12 17:08:41 by yufli            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINITALK_H
 # define MINITALK_H
 
 # include <signal.h>
 # include <unistd.h>
 # include <stdlib.h>
-# include "libft.h"
+# include "../libft/libft.h"
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1024
 # endif
 
-/* 服务器全局状态结构体（符合单全局变量要求）*/
-typedef struct	s_state
+typedef struct s_state
 {
-	char	*buffer;
-	size_t	buff_size;
-	size_t	index;
 	sig_atomic_t	current_bit;
 	sig_atomic_t	current_char;
+	char			*buffer;
+	size_t			buff_size;
+	size_t			index;
 }	t_state;
-/* 函数声明 */
-void	signal_handler(int sig, siginfo_t *info, void *ctx);
-void	handle_char(void);
+
 volatile t_state	g_state;
+
+void	init_state(void);
+void	handle_char(void);
+void	signal_handler(int sig, siginfo_t *info, void *ctx);
+void	send_char(pid_t pid, char c);
 
 #endif
