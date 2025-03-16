8bb1f2edbb59f3bd8be6e2dc6dd3aa7a83bad505
FranciscoLi2000 <francisco2000li@icloud.com>
2025-03-16 20:30:08 +0000

    archivos de cabecera

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yufli <yufli@student.42barcelona.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/15 16:04:12 by yufli             #+#    #+#             */
+/*   Updated: 2025/03/16 19:35:57 by yufli            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINITALK_H
 # define MINITALK_H
 
@@ -6,24 +18,15 @@
 # include <stdlib.h>
 # include "../libft/libft.h"
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1024
-# endif
-
-typedef struct s_state
+typedef struct s_server
 {
-	sig_atomic_t	current_bit;
-	sig_atomic_t	current_char;
-	char			*buffer;
-	size_t			buff_size;
-	size_t			index;
-}	t_state;
-
-volatile t_state	g_state;
-
-void	init_state(void);
-void	handle_char(void);
-void	signal_handler(int sig, siginfo_t *info, void *ctx);
-void	send_char(pid_t pid, char c);
+	char	current_char;
+	int		bit_count;
+}	t_server;
+void	send_bit(int bit, pid_t server_pid);
+void	send_char(char c, pid_t server_pid);
+void	sig_handler(int sig, siginfo_t *info, void *context);
+void	setup_signal(void);
+void	sig_acknowledge(int sig);
 
 #endif
--- includes/minitalk_bonus.h
+++ includes/minitalk_bonus.h
@@ -1,29 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk_bonus.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yufli <yufli@student.42barcelona.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/15 16:12:40 by yufli             #+#    #+#             */
+/*   Updated: 2025/03/16 20:33:37 by yufli            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINITALK_BONUS_H
 # define MINITALK_BONUS_H
 
 # include <signal.h>
-# include <unistd.h>
 # include <stdlib.h>
+# include <unistd.h>
 # include "../libft/libft.h"
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1024
-# endif
-
-typedef struct s_state
+typedef struct state
 {
-	sig_atomic_t	current_bit;
-	sig_atomic_t	current_char;
-	char			*buffer;
-	size_t			buff_size;
-	size_t			index;
+	pid_t	client_pid;
+	char	current_byte;
+	char	buffer[4];
+	int		bit_count;
+	int		byte_count;
 }	t_state;
-
-volatile t_state	g_state;
-
-void	init_state(void);
-void	handle_char(void);
-void	signal_handler(int sig, siginfo_t *info, void *ctx);
-void	send_char(pid_t pid, char c);
+void	ack_handler(int sig, siginfo_t *info, void *context);
+void	send_byte(char byte, pid_t server_pid);
+void	server_handler(int sig, siginfo_t *info, void *context);
+int		is_valid_utf8(const char *buffer, int len);
 
 #endif
